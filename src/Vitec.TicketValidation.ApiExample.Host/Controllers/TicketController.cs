8507d57001ed3bb2d805c847b90cd84a5eab72f2
Noah Maktabi <noah.maktabi@vitecsoftware.com>
2025-02-03 14:52:45 +0100

    ad some logs to help debug

--- src/Vitec.TicketValidation.ApiExample.Host/Controllers/TicketController.cs
+++ src/Vitec.TicketValidation.ApiExample.Host/Controllers/TicketController.cs
@@ -9,13 +9,15 @@ namespace Vitec.TicketValidation.ApiExample.Host.Controllers;
 [ApiController]
 [Route("api/tickets")] // Note: This is just for our reference implementation
 [Authorize]
-public class TicketController(TestTicketService ticketService) : ControllerBase
+public class TicketController(TestTicketService ticketService, ILogger<TicketController> logger) : ControllerBase
 {
     [HttpPost("validate")]
     public ActionResult<ValidateTicketResponse> ValidateTicket(ValidateTicketRequest request)
     {
+        logger.LogInformation("Got request to validate for {ticket} and reader {reader}", request.TicketId, request.ReaderId);
         if (string.IsNullOrEmpty(request.TicketId))
         {
+            logger.LogInformation("Returning bad request duo to empty ticket Id");
             return BadRequest(new ValidateTicketResponse
             {
                 Status = ValidationStatus.Error,
@@ -24,6 +26,7 @@ public class TicketController(TestTicketService ticketService) : ControllerBase
         }
 
         var response = ticketService.ValidateTicket(request.TicketId, request.ReaderId);
+        logger.LogInformation("Returning validate response with statues {s} and msg {m}", response.Status.ToString(), response.EndUserMessage);
         return Ok(response);
     }
 }
