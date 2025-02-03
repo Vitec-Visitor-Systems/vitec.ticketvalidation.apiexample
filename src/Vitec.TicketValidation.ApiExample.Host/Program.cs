4d8c6487811f4f298512abb07c7d3f2e740f1cb8
Noah Maktabi <noah.maktabi@vitecsoftware.com>
2025-02-03 10:30:15 +0100

    use swagger in prod

--- src/Vitec.TicketValidation.ApiExample.Host/Program.cs
+++ src/Vitec.TicketValidation.ApiExample.Host/Program.cs
@@ -6,11 +6,8 @@ builder.Services.AddServices(builder.Configuration);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
